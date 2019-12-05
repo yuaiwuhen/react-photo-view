2bafd1cb351037eb0067e3417c4b635b49951bd9
MinJieLiu <liumingyi_1@163.com>
2019-12-05 22:34:37 +0800

    Deploy Storybook to GitHub Pages

---
+++ precache-manifest.5c6214b7dc48d12bfcfa2842938bb1c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f041bbbc605c292f0ef8b467cc148a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3b9a64ba53d39201aba2.bundle.js"
+  },
+  {
+    "url": "runtime~main.3b9a64ba53d39201aba2.bundle.js"
+  },
+  {
+    "url": "vendors~main.3b9a64ba53d39201aba2.bundle.js"
+  },
+  {
+    "url": "vendors~main.3b9a64ba53d39201aba2.bundle.js.LICENSE"
+  }
+]);
